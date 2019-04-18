2c7936c90248aebb693804d8d4de959af0ec579b
Eugene Krishtal <Jeka1.1@mail.ru>
2019-04-18 19:11:11 +0300

    add action

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,9 @@
 import './index.sass';
 import './index.pug';
 
-
 export default () => {
+  const $campaing = $('.campaing')
+  $campaing.find('.js-create-order').on('click', () => {
+    console.log('aaaa');
+  })
 };
