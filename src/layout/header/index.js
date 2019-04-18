67260e397b537f86e459ed5b0ac557be221e47b5
Eugene Krishtal <Jeka1.1@mail.ru>
2019-04-18 21:03:21 +0300

    some fix

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,1 +1,9 @@
 import './header.sass';
+
+export default () => {
+  const $header = $('.header');
+  $header.find('.header__burger').on('click', (e) => {
+    console.log(1);
+    $(e.currentTarget).parents('header').toggleClass('header--small');
+  });
+}
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,8 +1,9 @@
 import './base';
 import './grid';
-import './header';
+import header from './header';
 import './footer';
 
 export default () => {
   console.log('init layout');
+  header();
 };
