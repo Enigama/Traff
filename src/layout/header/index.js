c0bddf46239a76440fd80f459245919973b18ab2
Eugene Krishtal <Jeka1.1@mail.ru>
2019-04-19 12:49:05 +0300

    fix header

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -3,7 +3,11 @@ import './header.sass';
 export default () => {
   const $header = $('.header');
   $header.find('.header__burger').on('click', (e) => {
-    console.log(1);
-    $(e.currentTarget).parents('header').toggleClass('header--small');
+    if ($(window).innerWidth() < 992) {
+      $(e.currentTarget).parents('header').toggleClass('header--mobile');
+    } else {
+      $(e.currentTarget).parents('header').toggleClass('header--small');
+    }
+
   });
 }
